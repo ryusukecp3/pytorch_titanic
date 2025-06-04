8e509254365dc2a036ca09db9de75457175854f9
ryk3110 <60802703+ryusukecp3@users.noreply.github.com>
2025-06-04 11:19:13 +0900

    Add notebook to run tests and update README

---
+++ net.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(4, 20)
+        self.fc2 = nn.Linear(20, 10)
+        self.fc3 = nn.Linear(10, 2)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        return F.log_softmax(x, dim=1)
